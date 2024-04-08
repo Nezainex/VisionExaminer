ee7e152f2de62416c2b1044f7fa5073047751142
Nezainex <111506983+Nezainex@users.noreply.github.com>
2024-04-09 01:10:12 +0300

    коммитю

---
+++ app/src/main/java/com/example/testapp13/RabkinResult.kt
@@ -0,0 +1,43 @@
+package com.example.testapp13
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "rabkin_results")
+data class RabkinResult(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    var patientProfileId: Int,
+    val score: Int,
+    val resultText: String
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeInt(patientProfileId)
+        parcel.writeInt(score)
+        parcel.writeString(resultText)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RabkinResult> {
+        override fun createFromParcel(parcel: Parcel): RabkinResult {
+            return RabkinResult(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RabkinResult?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
